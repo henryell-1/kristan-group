5d57dd65a0ecf909b39b5eb37667bd06b281dce4
krushme <kelsieberninger@gmail.com>
2021-12-30 14:06:08 -0600

    initial setup

---
+++ src/main/java/org/launchcode/GardenPlanner/GardenPlannerApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.GardenPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardenPlannerApplication.class, args);
+	}
+
+}
